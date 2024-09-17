a25935c64540106b625c7abb23cd6854b44abfdc
JeffDev17 <jeeff.araujo1@gmail.com>
2024-09-17 17:13:33 -0300

    fixed transaction deleting bug

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,55 +1,62 @@
 class TransactionsController < ApplicationController
-  before_action :authenticate_user!
   before_action :set_transaction, only: [:show, :edit, :update, :destroy]
+  before_action :set_categories, only: [:new, :create, :edit, :update]
 
   def index
+    # Shows current user's transactions, ordered by date with categories
     @transactions = current_user.account.transactions.includes(:category).order(created_at: :desc)
   end
 
   def show
+    # Uses the @transaction set by the before_action
   end
 
   def new
-    @transaction = current_user.account.transactions.build
-    @categories = Category.all
+    # Initializes a new transaction for the current user
+    @transaction = Transaction.new(account: current_user.account)
   end
 
   def create
-    @transaction = current_user.account.transactions.build(transaction_params)
-
+    @transaction = current_user.account.transactions.new(transaction_params)
     if @transaction.save
       redirect_to @transaction, notice: 'Transaction was successfully created.'
     else
-      @categories = Category.all
       render :new
     end
   end
 
   def edit
-    @categories = Category.all
+    # Uses the @transaction set by the before_action
   end
 
   def update
     if @transaction.update(transaction_params)
       redirect_to @transaction, notice: 'Transaction was successfully updated.'
     else
-      @categories = Category.all
       render :edit
     end
   end
 
   def destroy
     @transaction.destroy
-    redirect_to transactions_url, notice: 'Transaction was successfully destroyed.'
+    flash[:success] = "Transaction was successfully deleted."
+    redirect_to transactions_path
   end
 
   private
 
   def set_transaction
+    # Loads the specific transaction for the current user
     @transaction = current_user.account.transactions.find(params[:id])
   end
 
+  def set_categories
+    # Loads all categories for the transactions
+    @categories = Category.all
+  end
+
   def transaction_params
+    # Permits necessary parameters for creating/updating a transaction
     params.require(:transaction).permit(:description, :amount, :category_id)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def br_currency(amount)
+    number_to_currency(amount, unit: "R$", separator: ",", delimiter: ".", format: "%u %n")
+  end
 end
